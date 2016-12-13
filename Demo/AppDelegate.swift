b0d691ed57ffffe3f6110bb5e8bff9cd0b64f06d
Jakub Kuzimski <qzim@me.com>
2016-12-13 20:27:27 +0100

    Initial commit

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  Created by Jakub Kuzimski on 13.12.2016.
+//  Copyright (c) 2016 Jakub Kuzimski. All rights reserved.
+//
+
+import UIKit
+
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Jakub Kuzimski on 13.12.2016.
+//  Copyright (c) 2016 Jakub Kuzimski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
